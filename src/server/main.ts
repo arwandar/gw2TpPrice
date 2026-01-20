479a86b02fb5b8b4a1b169ced5955226ca288a85
Arwandar <arwandar@gmail.com>
2026-01-20 09:29:14 +0100

    move docker docker build from alpine

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
-const cache = new Map<string, { data: any; expires: number }>();
-
 const app = express();
+
+type CacheEntry = {
+  data: any;
+  expires: number;
+};
+
+const cache = new Map<string, CacheEntry>();
 
 app.get("/api/gw2tp/:id", async (req, resp) => {
   const id = req.params.id;
@@ -20,16 +25,16 @@ app.get("/api/gw2tp/:id", async (req, resp) => {
 
     cache.set(id, {
       data,
-      expires: now + 360_000, 
+      expires: now + 360_000,
     });
 
     resp.json(data);
   } catch (e) {
-    console.error(e)
+    console.error(e);
     resp.status(502).json({ error: "upstream failed" });
   }
 });
 
 ViteExpress.listen(app, 5174, () =>
-  console.log("Server is listening on port 5174...")
+  console.log("Server is listening on port 5174..."),
 );
